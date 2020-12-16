e90fd94dd747e4780bde3eb076b0cd5540759af1
co5ta <costamonzili@gmail.com>
2020-12-16 11:47:19 +0100

    Add question label and option buttons

--- Quizzes/ViewController.m
+++ Quizzes/ViewController.m
@@ -18,6 +18,11 @@ typedef NS_CLOSED_ENUM(NSInteger, GameState) {
 @interface ViewController ()
 /// Button to start a new game
 @property (weak, nonatomic) IBOutlet UIButton *playButton;
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *option1Button;
+@property (weak, nonatomic) IBOutlet UIButton *option2Button;
+@property (weak, nonatomic) IBOutlet UIButton *option3Button;
+@property (weak, nonatomic) IBOutlet UIButton *option4Button;
 @end
 
 @implementation ViewController
@@ -31,15 +36,29 @@ typedef NS_CLOSED_ENUM(NSInteger, GameState) {
 /// Configures the view controller
 - (void)configure {
     [self setGameState:starting];
-    self.playButton.layer.borderWidth = 1;
-    self.playButton.layer.borderColor = UIColor.whiteColor.CGColor;
-    self.playButton.contentEdgeInsets = UIEdgeInsetsMake(10, 20, 10, 20);
-    self.playButton.layer.cornerRadius = 10;
+    [self setUpPlayButton];
+    [self setUpOptionButtons];
 }
 
 /// Updates the views according to the game state
 - (void)setGameState: (GameState) state {
     [self.playButton setHidden:state == starting ? false : true];
+}
+
+/// Sets up play button
+- (void)setUpPlayButton {
+    self.playButton.layer.borderWidth = 1;
+    self.playButton.layer.borderColor = UIColor.whiteColor.CGColor;
+    self.playButton.layer.cornerRadius = 10;
+    self.playButton.contentEdgeInsets = UIEdgeInsetsMake(10, 20, 10, 20);
+}
+
+/// Sets up option buttons style
+- (void)setUpOptionButtons {
+    for (UIButton *button in @[self.option1Button, self.option2Button]) {
+        button.layer.cornerRadius = 10;
+        button.contentEdgeInsets = UIEdgeInsetsMake(10, 0, 10, 0);
+    }
 }
 
 @end
