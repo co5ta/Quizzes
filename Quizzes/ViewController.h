a1fa895d0e2b859e5211ced8a639bc7a32343bec
co5ta <costamonzili@gmail.com>
2021-01-05 22:08:17 +0100

    Reorganize the code

--- Quizzes/ViewController.h
+++ Quizzes/ViewController.h
@@ -6,9 +6,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Question.h"
 
 @interface ViewController : UIViewController
 
+/// Enum of game state
+typedef NS_CLOSED_ENUM(NSInteger, GameState) {
+    starting = 1,
+    loading,
+    playing,
+    over,
+    error
+};
+
+/// Button that starts a new game
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
+/// Label that contains the question
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+/// Buttons that contains the available responses
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *optionButtons;
+/// Object that contains data about the question
+@property Question *question;
+/// Score of the player
+@property NSInteger score;
 
 @end
 
--- Quizzes/ViewController.m
+++ Quizzes/ViewController.m
@@ -6,68 +6,21 @@
 //
 
 #import "ViewController.h"
-#import "Question.h"
-
-/// Enum of game state
-typedef NS_CLOSED_ENUM(NSInteger, GameState) {
-    starting = 1,
-    loading,
-    playing,
-    over,
-    error
-};
-
-@interface ViewController ()
-/// Button to start a new game
-@property (weak, nonatomic) IBOutlet UIButton *playButton;
-@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
-@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *optionButtons;
-@property Question *question;
-@end
 
 @implementation ViewController
 
 /// Called after the controller's view is loaded into memory
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.question = [Question new];
     [self configure];
+    [self setGameState:starting];
 }
 
 /// Configures the view controller
 - (void)configure {
+    self.question = [Question new];
     [self setUpPlayButton];
     [self setUpOptionButtons];
-    [self setGameState:starting];
-}
-
-/// Updates the views according to the game state
-- (void)setGameState: (GameState) state {
-    switch (state) {
-        case starting:
-            [self.playButton setHidden:NO];
-            [self.questionLabel setAlpha:0];
-            for (UIButton *button in self.optionButtons) {
-                [button setAlpha:0];
-                button.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.1, 0.1);
-            }
-            break;
-        case loading:
-            [self.playButton setHidden:YES];
-            [self.questionLabel setAlpha:0];
-            for (UIButton *button in self.optionButtons) {
-                [button setAlpha:0];
-                button.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.1, 0.1);
-                [button setTitle:@"" forState:UIControlStateNormal];
-                [button setBackgroundColor:UIColor.systemTealColor];
-            }
-            break;
-        case playing:
-            [self updateQuestion];
-            break;
-        default:
-            break;
-    }
 }
 
 /// Sets up play button
@@ -86,30 +39,55 @@ typedef NS_CLOSED_ENUM(NSInteger, GameState) {
     }
 }
 
-/// Action which launch a game
-- (IBAction)startGame:(UIButton *)sender {
-    [self getQuestion];
+/// Updates the views according to the game state
+- (void)setGameState: (GameState) state {
+    switch (state) {
+        case starting:
+            [self showHomeScreen];
+            break;
+        case loading:
+            [self showLoadingScreen];
+            break;
+        case playing:
+            [self updateQuestionDisplayed];
+            break;
+        default:
+            break;
+    }
 }
 
-/// Fetch a question from the API
-- (void)getQuestion {
+/// Configures the UI to show the home screen
+- (void)showHomeScreen {
+    self.score = 0;
+    [self.playButton setHidden:NO];
+    [self.questionLabel setAlpha:0];
+    for (UIButton *button in self.optionButtons) {
+        [button setAlpha:0];
+        button.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.1, 0.1);
+    }
+}
+
+/// Configures the UI to show the loading screen
+- (void)showLoadingScreen {
+    [self.playButton setHidden:YES];
+    [self.questionLabel setAlpha:0];
+    for (UIButton *button in self.optionButtons) {
+        [button setAlpha:0];
+        button.transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.1, 0.1);
+        [button setTitle:@"" forState:UIControlStateNormal];
+        [button setBackgroundColor:UIColor.systemTealColor];
+    }
+}
+
+/// Action which launch a game
+- (IBAction)playButtonTapped:(UIButton *)sender {
+    [self getNewQuestion];
+}
+
+/// Fetchs a new question to display
+- (void)getNewQuestion {
     [self setGameState:loading];
-    NSURL *url = [NSURL URLWithString:@"https://opentdb.com/api.php?amount=1&encode=url3986"];
-    NSError *error;
-    
-    NSData *data = [NSData dataWithContentsOfURL:url options:0 error:&error];
-    if (error != nil) {
-        NSLog(@"Fatal error 1: %@", error.localizedDescription);
-        exit(0);
-    }
-    
-    NSDictionary *dico = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
-    if (error != nil) {
-        NSLog(@"Fatal error 2: %@", error.localizedDescription);
-        exit(0);
-    }
-    
-    NSDictionary *results = dico[@"results"][0];
+    NSDictionary *results = [self requestQuestion];
     self.question.title = results[@"question"];
     self.question.response = results[@"correct_answer"];
     self.question.options = [NSMutableArray arrayWithArray:results[@"incorrect_answers"]];
@@ -119,13 +97,29 @@ typedef NS_CLOSED_ENUM(NSInteger, GameState) {
     [self setGameState:playing];
 }
 
-/// updates the question displayed and the possible responses
-- (void)updateQuestion {
+/// Requests a new question to the API
+- (NSDictionary*)requestQuestion {
+    NSURL *url = [NSURL URLWithString:@"https://opentdb.com/api.php?amount=1&encode=url3986"];
+    NSError *error;
+    NSData *data = [NSData dataWithContentsOfURL:url options:0 error:&error];
+    if (error != nil) {
+        NSLog(@"Fatal error 1: %@", error.localizedDescription);
+        exit(0);
+    }
+    NSDictionary *dico = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    if (error != nil) {
+        NSLog(@"Fatal error 2: %@", error.localizedDescription);
+        exit(0);
+    }
+    return dico[@"results"][0];
+}
+
+/// Updates the question data
+- (void)updateQuestionDisplayed {
     [self.questionLabel setText:[self.question.title stringByRemovingPercentEncoding]];
     for (int i = 0; i < [self.question.options count]; i++) {
         [self.optionButtons[i] setTitle:[self.question.options[i] stringByRemovingPercentEncoding] forState:UIControlStateNormal];
     }
-    
     [UIView animateWithDuration:0.5 animations:^{
         [self.questionLabel setAlpha:1];
     } completion:^(BOOL finished) {
@@ -148,7 +142,7 @@ typedef NS_CLOSED_ENUM(NSInteger, GameState) {
     BOOL goodAnswer = sender.currentTitle == self.question.response ? YES : NO;
     [sender setBackgroundColor:goodAnswer ? UIColor.systemGreenColor : UIColor.systemRedColor];
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self getQuestion];
+        [self getNewQuestion];
     });
 }
 
